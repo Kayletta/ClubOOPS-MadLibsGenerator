8cf8cd548bad32b944ebeeddeacf27f7bf6b4cf4
Jennifer Chesnes <chesnes7620@student.cptc.edu>
2016-03-09 16:36:03 -0800

    Project Start
    Beginning Windows Forms App C# Mad Libs Generator

---
+++ MadLibs_Generator/MadLibs_Generator/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MadLibs_Generator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // Select mad lib via radio button
+        // Hide all groupbox2-4 until selection is made
+        // groupbox should contain the list of parts of speech and txtboxes
+    }
+}
